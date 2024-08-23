41bfca0cf32b2f18b45b138967e2b9bfcb58a183
Rai <raitorresx@gmail.com>
2024-08-22 22:19:24 -0300

    commit

--- ambiente/app.py
+++ ambiente/app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
-from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores.faiss import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 import streamlit as st
